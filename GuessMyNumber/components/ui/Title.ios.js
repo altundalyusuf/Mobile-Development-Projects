a9af9b5b3c99d8e03e2934e90e62c3b503ee1f58
Yusuf Altundal <79724222+altundalyusuf@users.noreply.github.com>
2025-11-26 18:27:07 +0300

    adaptive platform & device sizes

--- GuessMyNumber/components/ui/Title.ios.js
+++ GuessMyNumber/components/ui/Title.ios.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, Platform } from "react-native";
 
 function Title({ children }) {
   return <Text style={styles.title}>{children}</Text>;
@@ -10,11 +10,13 @@ const styles = StyleSheet.create({
   title: {
     fontFamily: "open-sans-bold",
     fontSize: 24,
-    fontWeight: "bold",
+    // fontWeight: "bold",
     color: "white",
     textAlign: "center",
-    borderWidth: 2,
-    borderColor: "white",
+    // borderWidth: Platform.OS === "android" ? 2 : 0,
+    // borderWidth: Platform.select({ ios: 0, android: 2 }),
     padding: 12,
+    maxWidth: "80%",
+    width: 300,
   },
 });
